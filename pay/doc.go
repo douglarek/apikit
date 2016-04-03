b6a0e4f1dc62e8edaeeee519e710ec9872a1c771
Lingchao Xin <xinlingchao@meican.com>
2016-04-03 16:44:44 +0800

    Unionpay: readme

--- pay/doc.go
+++ pay/doc.go
@@ -32,5 +32,19 @@ Example:
 	r.SellerEmail = ""
 	r.Sign = string(s.Sign(r, []byte(``)))
 	fmt.Println(s.PayURL(r))
+
+	r := union.DefaultOrderReq()
+	r.CertID = "" // openssl pkcs12 -in acp_prod_sign.pfx -clcerts -nokeys -out key.cert
+	r.BackURL = "http://127.0.0.1"
+	r.MerID = ""
+	r.OrderID = strconv.FormatInt(time.Now().UnixNano(), 10)
+	r.FrontURL = "http://127.0.0.1"
+	r.OrderDesc = "desc"
+	secret := `` // openssl pkcs12 -in acp_prod_sign.pfx -nocerts -nodes -out key.pem
+	s := union.Sign(req, []byte(secret))
+	r.Signature = s
+	resp := union.AppConsume(r)
+	public := `` // acp_prod_verify_sign.cer
+	fmt.Println(union.Verify(resp, []byte(public), []byte(resp.Signature)))
 */
 package pay
